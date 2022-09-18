f9682882803f4c07af76cb11cbc8ea2999f4f04b
Raist90 <manuel.detoma@gmail.com>
2022-09-18 12:53:38 +0200

    added icons and read me

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Button from '../components/Button'
 import Footer from '../components/Footer'
+import { TiSocialGithubCircular, TiSocialLinkedinCircular } from 'react-icons/ti'
 
 export default function Home({ title = "Home" }) {
   return (
@@ -18,6 +19,11 @@ export default function Home({ title = "Home" }) {
           <div className={styles.portraitContainer}></div>
           <h1 className={styles.homeContainer__title}>MANUEL DE TOMA</h1>
           <p className={styles.homeContainer__small}><em>Born in Italy, 32 years old</em></p>
+          <div className={styles.homeContainer__icons}>
+            <a href="https://github.com/Raist90" target="_blank" title="Github profile"><TiSocialGithubCircular /></a>
+            <a href="https://www.linkedin.com/in/manuel-de-toma/" target="_blank" title="LinkedIn profile"><TiSocialLinkedinCircular /></a>
+          </div>
+          <hr />
           <h4>Frontend Dev</h4>
           <p>I am an experienced web developer interested in projects that require both creative and analytical thinking, and I prioritize user experience in my work.
           </p>
